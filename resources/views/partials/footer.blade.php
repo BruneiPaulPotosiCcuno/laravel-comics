f17a104f7df51c354cf4f0bd5f2621ea7dac7c5e
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-05-23 22:02:25 +0200

    add-footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,78 @@
+<footer>
+    <div class="container py-5 container_logo">
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-4 mb-4">
+                <div class="subtitle py-3 fs-5 font-weight-bold">DC COMICS</div>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#">Characters</a></li>
+                    <li class="mb-2"><a href="#">Comics</a></li>
+                    <li class="mb-2"><a href="#">Movies</a></li>
+                    <li class="mb-2"><a href="#">TV</a></li>
+                    <li class="mb-2"><a href="#">Games</a></li>
+                    <li class="mb-2"><a href="#">Videos</a></li>
+                    <li class="mb-2"><a href="#">News</a></li>
+                </ul>
+                <div class="subtitle py-3 fs-5 font-weight-bold">SHOP</div>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#">Shop DC</a></li>
+                    <li class="mb-2"><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4 mb-4">
+                <div class="subtitle py-3 fs-5 font-weight-bold">DC</div>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#">Terms of Use</a></li>
+                    <li class="mb-2"><a href="#">Privacy Policy</a></li>
+                    <li class="mb-2"><a href="#">Ad Choices</a></li>
+                    <li class="mb-2"><a href="#">Advertising</a></li>
+                    <li class="mb-2"><a href="#">Jobs</a></li>
+                    <li class="mb-2"><a href="#">Subscriptions</a></li>
+                    <li class="mb-2"><a href="#">Talent Workshops</a></li>
+                    <li class="mb-2"><a href="#">CPSC Certificates</a></li>
+                    <li class="mb-2"><a href="#">Ratings</a></li>
+                    <li class="mb-2"><a href="#">Shop Help</a></li>
+                    <li class="mb-2"><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4 mb-4">
+                <div class="subtitle py-3 fs-5 font-weight-bold">SITES</div>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#">DC</a></li>
+                    <li class="mb-2"><a href="#">MAD Magazine</a></li>
+                    <li class="mb-2"><a href="#">DC Kids</a></li>
+                    <li class="mb-2"><a href="#">DC Universe</a></li>
+                    <li class="mb-2"><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-final py-4 bg-dark text-white">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-12 col-md-6 mb-3 mb-md-0 text-center text-md-start">
+                    <button type="button" class="btn btn-primary">SIGN-UP NOW!</button>
+                </div>
+                <div class="col-12 col-md-6 text-center text-md-end">
+                    <div class="subtitle font-weight-bold fs-5">FOLLOW US</div>
+                    <ul class="list-inline mt-2">
+                        <li class="list-inline-item mx-2">
+                            <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook"></a>
+                        </li>
+                        <li class="list-inline-item mx-2">
+                            <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope"></a>
+                        </li>
+                        <li class="list-inline-item mx-2">
+                            <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest"></a>
+                        </li>
+                        <li class="list-inline-item mx-2">
+                            <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter"></a>
+                        </li>
+                        <li class="list-inline-item mx-2">
+                            <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
